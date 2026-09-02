1cd5e0a13b01e3b8deed09e5f7707778c955cf0d
Naveen Venkat <81929600+naveenvenk17@users.noreply.github.com>
2026-09-03 01:01:22 +0530

    Cover overlap checks for an already placed macro
    Signed-off-by: Naveen Venkat <archgen.guest@nyayanidhi.in>

---
+++ src/mpl/test/place_macro_right_angle_rotation.tcl
@@ -0,0 +1,20 @@
+# Test if a macro on R90 orientation is correctly placed by place_macro.
+source "helpers.tcl"
+
+read_lef "./Nangate45/Nangate45.lef"
+read_lef "./testcases/place_macro_right_angle_rotation.lef"
+
+read_def "./testcases/place_macro_right_angle_rotation.def"
+
+# Exercise placement overlap checks when the target macro is already placed.
+set block [ord::get_db_block]
+set macro [$block findInst macro]
+$macro setLocation 40000 40000
+$macro setPlacementStatus PLACED
+
+place_macro -macro_name macro -location {20 20} -orientation R90
+
+set def_file [make_result_file place_macro_right_angle_rotation.def]
+write_def $def_file
+
+diff_files place_macro_right_angle_rotation.defok $def_file
